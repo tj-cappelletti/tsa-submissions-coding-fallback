766a491d46c167470dcaef52e0e331a34e313aa3
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-04-16 14:00:36 -0400

    Updated seed data to set first two judges

--- TsaSubmissions.Web/Data/SeedData.cs
+++ TsaSubmissions.Web/Data/SeedData.cs
@@ -17,24 +17,24 @@ public static class SeedData
         {
             var hasher = new PasswordHasher<AppUser>();
 
-            var judge = new AppUser { Username = "judge", Role = AppRole.Judge };
-            judge.PasswordHash = hasher.HashPassword(judge, "judge123!");
+            var firstJudge = new AppUser { Username = "judge01", Role = AppRole.Judge };
+            firstJudge.PasswordHash = hasher.HashPassword(firstJudge, "judge123!");
 
-            var participant = new AppUser { Username = "participant", Role = AppRole.Participant };
-            participant.PasswordHash = hasher.HashPassword(participant, "participant123!");
+            var secondJudge = new AppUser { Username = "judge02", Role = AppRole.Judge };
+            secondJudge.PasswordHash = hasher.HashPassword(secondJudge, "judge123!"); 
 
-            db.Users.AddRange(judge, participant);
+            db.Users.AddRange(firstJudge, secondJudge);
         }
 
         if (!await db.Problems.AnyAsync())
-        {
-            var problem = new Problem
             {
-                Title = "FizzBuzz Extended",
-                DescriptionMarkdown = "# FizzBuzz Extended\n\nWrite a program that prints numbers 1-100.\n\n- Multiples of 3 print `Fizz`\n- Multiples of 5 print `Buzz`\n- Multiples of both print `FizzBuzz`"
-            };
+                var problem = new Problem
+                {
+                    Title = "FizzBuzz Extended",
+                    DescriptionMarkdown = "# FizzBuzz Extended\n\nWrite a program that prints numbers 1-100.\n\n- Multiples of 3 print `Fizz`\n- Multiples of 5 print `Buzz`\n- Multiples of both print `FizzBuzz`"
+                };
 
-            problem.StarterCodes = new List<ProblemStarterCode>
+                problem.StarterCodes = new List<ProblemStarterCode>
             {
                 new() { Language = SupportedLanguage.Cpp, Code = "#include <iostream>\nint main(){\n    // TODO\n    return 0;\n}" },
                 new() { Language = SupportedLanguage.CSharp, Code = "using System;\nclass Program{\n    static void Main(){\n        // TODO\n    }\n}" },
@@ -43,9 +43,10 @@ public static class SeedData
                 new() { Language = SupportedLanguage.Python, Code = "def main():\n    # TODO\n    pass\n\nif __name__ == '__main__':\n    main()" }
             };
 
-            db.Problems.Add(problem);
-        }
+                db.Problems.Add(problem);
+            }
 
-        await db.SaveChangesAsync();
+            await db.SaveChangesAsync();
+        }
     }
 }
