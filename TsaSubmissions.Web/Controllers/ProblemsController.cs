cf19642b4c3dc09efcd9302362ec28e3fc72ca25
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-04-16 13:28:51 -0400

    Added ability for participants to view their own submissions

--- TsaSubmissions.Web/Controllers/ProblemsController.cs
+++ TsaSubmissions.Web/Controllers/ProblemsController.cs
@@ -143,4 +143,87 @@ public class ProblemsController(ApplicationDbContext dbContext, IOptions<EventSe
 
         return userId;
     }
+
+    [Authorize(Roles = nameof(AppRole.Participant))]
+    public async Task<IActionResult> MySubmissions(int problemId)
+    {
+        var problem = await dbContext.Problems.SingleOrDefaultAsync(p => p.Id == problemId);
+        if (problem is null)
+        {
+            return NotFound();
+        }
+
+        var participantId = GetCurrentUserId();
+        var submissions = await dbContext.Submissions
+            .Where(s => s.ProblemId == problemId && s.ParticipantId == participantId)
+            .OrderByDescending(s => s.UploadedAtUtc)
+            .ThenByDescending(s => s.Id)
+            .ToListAsync();
+
+        var vm = new JudgeProblemSubmissionsViewModel
+        {
+            Problem = problem,
+            LatestSubmissions = submissions
+        };
+
+        return View(vm);
+    }
+
+    [Authorize(Roles = nameof(AppRole.Participant))]
+    public async Task<IActionResult> ViewSubmission(int submissionId)
+    {
+        var participantId = GetCurrentUserId();
+        var submission = await dbContext.Submissions
+            .Include(s => s.Participant)
+            .Include(s => s.Problem)
+            .SingleOrDefaultAsync(s => s.Id == submissionId && s.ParticipantId == participantId);
+
+        if (submission is null)
+        {
+            return NotFound();
+        }
+
+        string displayCode;
+        try
+        {
+            displayCode = new System.Text.UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true)
+                .GetString(submission.FileContent);
+        }
+        catch (System.Text.DecoderFallbackException)
+        {
+            displayCode = "// Unable to display this submission as UTF-8 text.";
+        }
+
+        var vm = new SubmissionCodeViewModel
+        {
+            Submission = submission,
+            DisplayCode = displayCode,
+            PrismLanguageClass = submission.Language switch
+            {
+                SupportedLanguage.Cpp => "language-cpp",
+                SupportedLanguage.CSharp => "language-csharp",
+                SupportedLanguage.Java => "language-java",
+                SupportedLanguage.JavaScript => "language-javascript",
+                SupportedLanguage.Python => "language-python",
+                _ => "language-clike"
+            }
+        };
+
+        return View(vm);
+    }
+
+    [Authorize(Roles = nameof(AppRole.Participant))]
+    public async Task<IActionResult> DownloadSubmission(int submissionId)
+    {
+        var participantId = GetCurrentUserId();
+        var submission = await dbContext.Submissions
+            .SingleOrDefaultAsync(s => s.Id == submissionId && s.ParticipantId == participantId);
+
+        if (submission is null)
+        {
+            return NotFound();
+        }
+
+        return File(submission.FileContent, submission.ContentType, submission.FileName);
+    }
 }
